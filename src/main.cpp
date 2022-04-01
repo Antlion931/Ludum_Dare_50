92952266e1bb9cdea1f25ca423909553b0743f58
Albert Kolodziejki <albertk931@protonmail.com>
2022-04-01 23:49:11 +0200

    Poprawa maina

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,14 @@
 #include "MovingCircle.hpp"
 #include "ColoredButton.hpp"
 
+enum
+{
+MAIN_MENU,
+TEST_PLAY,
+SETTINGS,
+GAME
+};
+
 int main()
 {
     Resolution resolution(Resolution::resolution::_1280x720);
@@ -27,69 +35,101 @@ int main()
         return 0;
     }
 
-    sf::Text buttonText("place holder", font);
-    buttonText.setCharacterSize(20);
-
-    std::shared_ptr<MovingCircle> circle = std::make_shared<MovingCircle>(MovingCircle({ 100,100 }, 100));
-    circle->setVelocity({ 10.f,10.f });
+    sf::Text ComisBookText("place holder", font);
+    ComisBookText.setCharacterSize(20);
 
     std::unique_ptr<LevelLoader> root = std::make_unique<LevelLoader>(LevelLoader());
     root->setName("root");
 
+    //================================================================================================MAIN MENU
     std::shared_ptr<Node> mainMenu = std::make_shared<Node>(Node());
-    std::shared_ptr<Node> secondaryMenu = std::make_shared<Node>(Node());
-        
     root->addLevel(mainMenu);
-    root->addLevel(secondaryMenu);
 
-    secondaryMenu->addChild(circle);
+    std::shared_ptr<Container> mainMenuButtons = std::make_shared<Container>();
+    mainMenuButtons->setName("Resolution Settings");
+    mainMenu->addChild(mainMenuButtons);
 
-    buttonText.setString("1280 x 720");
-    std::shared_ptr<ColoredButton> _1280x720button = std::make_shared<ColoredButton>(ColoredButton({ 50,200 }, { 200,100 }, buttonText));
+    ComisBookText.setString("PLAY");
+    ComisBookText.setCharacterSize(90);
+    std::shared_ptr<ColoredButton> playButton = std::make_shared<ColoredButton>(ColoredButton({ 490,50 }, { 300,100 }, ComisBookText));
+    playButton->setName("Play button");
+    playButton->setOnNotHoveredButtonStyle(Style(sf::Color(200, 200, 200), sf::Color(180, 180, 180), 10));
+    playButton->setOnEntredButtonStyle(Style(sf::Color(210, 210, 210), sf::Color(190, 190, 190), 20));
+    playButton->setOnPressedButtonStyle(Style(sf::Color(190, 190, 190), sf::Color(170, 170, 170), 5));
+    playButton->setOnEnteredFontStyle(Style(sf::Color::Yellow, sf::Color::Black, 4));
+    playButton->setOnPressedFontStyle(Style(sf::Color::Yellow, sf::Color::Black, 4));
+    mainMenuButtons->addChild(playButton);
+
+    ComisBookText.setString("SETTINGS");
+    ComisBookText.setCharacterSize(55);
+    std::shared_ptr<ColoredButton> settingsButton = std::make_shared<ColoredButton>(ColoredButton({ 490,250 }, { 300,100 }, ComisBookText));
+    settingsButton->setName("settings button");
+    settingsButton->setOnNotHoveredButtonStyle(Style(sf::Color(200, 200, 200), sf::Color(180, 180, 180), 20));
+    settingsButton->setOnEntredButtonStyle(Style(sf::Color(210, 210, 210), sf::Color(190, 190, 190), 30));
+    settingsButton->setOnPressedButtonStyle(Style(sf::Color(190, 190, 190), sf::Color(170, 170, 170), 10));
+    settingsButton->setOnEnteredFontStyle(Style(sf::Color::Yellow, sf::Color::Black, 4));
+    settingsButton->setOnPressedFontStyle(Style(sf::Color::Yellow, sf::Color::Black, 4));
+    mainMenuButtons->addChild(settingsButton);
+
+    ComisBookText.setString("TEST");
+    ComisBookText.setCharacterSize(90);
+    std::shared_ptr<ColoredButton> testButton = std::make_shared<ColoredButton>(ColoredButton({ 490,450 }, { 300,100 }, ComisBookText));
+    testButton->setName("Test button");
+    testButton->setOnNotHoveredButtonStyle(Style(sf::Color(200, 200, 200), sf::Color(180, 180, 180), 10));
+    testButton->setOnEntredButtonStyle(Style(sf::Color(210, 210, 210), sf::Color(190, 190, 190), 20));
+    testButton->setOnPressedButtonStyle(Style(sf::Color(190, 190, 190), sf::Color(170, 170, 170), 5));
+    testButton->setOnEnteredFontStyle(Style(sf::Color::Yellow, sf::Color::Black, 4));
+    testButton->setOnPressedFontStyle(Style(sf::Color::Yellow, sf::Color::Black, 4));
+    mainMenuButtons->addChild(testButton);
+
+    //====================================================================================================TESTING
+    std::shared_ptr<Node> test = std::make_shared<Node>(Node());
+    root->addLevel(test);
+
+    //jak chcesz coś przetestować to twórz obiekty tutaj
+
+    //===================================================================================================SETTINGS
+
+    std::shared_ptr<Node> settings = std::make_shared<Node>(Node());
+    root->addLevel(settings);
+
+    std::shared_ptr<Container> resolutionSettings = std::make_shared<Container>();
+    resolutionSettings->setName("Resolution Settings");
+    settings->addChild(resolutionSettings);
+
+    resolutionSettings->translate({ 100, 100 });
+        
+    ComisBookText.setString("_1280 x 720");
+    std::shared_ptr<ColoredButton> _1280x720button = std::make_shared<ColoredButton>(ColoredButton({ 50,200 }, { 200,100 }, ComisBookText));
     _1280x720button->setName("_1280x720button");
+    resolutionSettings->addChild(_1280x720button);
 
-    buttonText.setString("1336 x 768");
-    std::shared_ptr<TextButton> _1336x768button = std::make_shared<TextButton>(TextButton({ 300,200 }, { 200,100 }, buttonText));
+    ComisBookText.setString("1336 x 768");
+    std::shared_ptr<ColoredButton> _1336x768button = std::make_shared<ColoredButton>(ColoredButton({ 300,200 }, { 200,100 }, ComisBookText));
     _1336x768button->setName("_1336x768button");
+    resolutionSettings->addChild(_1336x768button);
 
-    buttonText.setString("1600 x 900");
-    std::shared_ptr<TextButton> _1600x900button = std::make_shared<TextButton>(TextButton({ 550,200 }, { 200,100 }, buttonText));
+    ComisBookText.setString("1600 x 900");
+    std::shared_ptr<ColoredButton> _1600x900button = std::make_shared<ColoredButton>(ColoredButton({ 550,200 }, { 200,100 }, ComisBookText));
     _1600x900button->setName("_1600x900button");
+    resolutionSettings->addChild(_1600x900button);
 
-    buttonText.setString("1900 x 1080");
-    std::shared_ptr<TextButton> _1900x1080button = std::make_shared<TextButton>(TextButton({ 800,200 }, { 200,100 }, buttonText));
+    ComisBookText.setString("1900 x 1080");
+    std::shared_ptr<ColoredButton> _1900x1080button = std::make_shared<ColoredButton>(ColoredButton({ 800,200 }, { 200,100 }, ComisBookText));
     _1900x1080button->setName("_1900x1080button");
+    resolutionSettings->addChild(_1900x1080button);
+
+    ComisBookText.setString("GO BACK");
+    std::shared_ptr<ColoredButton> goBackButton = std::make_shared<ColoredButton>(ColoredButton({ 1050,200 }, { 200,100 }, ComisBookText));
+    _1900x1080button->setName("Go Back Button");
+    resolutionSettings->addChild(goBackButton);
+
     
+    //========================================================================================GAME
+    std::shared_ptr<Node> game = std::make_shared<Node>(Node());
+    root->addLevel(game);
 
-    std::shared_ptr<MouseChangeableProgressbar> progressbar = std::make_shared<MouseChangeableProgressbar>(1000.0f, 50.0f, sf::Color(100, 100, 100), sf::Color(200, 200, 200));
-    progressbar->setName("progressbar");
-
-    std::shared_ptr<Container> container = std::make_shared<Container>();
-    container->setName("container");
-
-    container->addChild(_1280x720button);
-    container->addChild(_1336x768button);
-    container->addChild(_1600x900button);
-    container->addChild(_1900x1080button);
-    mainMenu->addChild(progressbar);
-    mainMenu->addChild(container);
-
-    container->translate({ 100, 100 });
-    progressbar->translate({ 100, 100 });
-
-    sf::Text text("przycisk", font);
-    text.setFillColor(sf::Color::Red);
-    text.setCharacterSize(50);
-
-    mainMenu->printTree();
-    TextureLoader tl;
-
-    if (!font.loadFromFile("res/Comic_Book.otf"))
-    {
-        std::cerr << "font errore!\n";
-        return 0;
-    }
-
+    //=========================================================================================GAME LOOP
     sf::Clock deltaClock;
     while (window.isOpen())
     {
@@ -114,9 +154,6 @@ int main()
                 break;
             }
         }
-
-
-        progressbar->update(window);
 
         if (_1280x720button->isPressed(window))
         {
@@ -146,9 +183,29 @@ int main()
             _1280x720button->printDebug();
         }
 
+        if(playButton->isPressed(window))
+        {
+            root->setLevel(GAME);
+        }
+
+        if(settingsButton->isPressed(window))
+        {
+            root->setLevel(SETTINGS);
+        }
+
+        if(testButton->isPressed(window))
+        {
+            root->setLevel(TEST_PLAY);
+        }
+
+        if(goBackButton->isPressed(window))
+        {
+            root->setLevel(MAIN_MENU);
+        }
+
         sf::Time delta = deltaClock.restart();
         window.clear();
-        
+
         root->update(delta);
         root->draw(window);
 
