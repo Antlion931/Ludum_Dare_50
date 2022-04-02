35e54289a824fc98953b0c6d113d762a074917a1
Grubre <ogrodowczykjakub@gmail.com>
2022-04-02 20:51:08 +0200

    Code cleanup w mainie

--- src/main.cpp
+++ src/main.cpp
@@ -253,24 +253,8 @@ int main()
         }
 
         sf::Time delta = deltaClock.restart();
+        
         window.clear();
-
-        sf::VertexArray quad(sf::Quads, 4);
-
-        // define it as a rectangle, located at (10, 10) and with size 100x100
-        quad[0].position = sf::Vector2f(10.f, 10.f);
-        quad[1].position = sf::Vector2f(110.f, 10.f);
-        quad[2].position = sf::Vector2f(110.f, 110.f);
-        quad[3].position = sf::Vector2f(10.f, 110.f);
-
-        // define its texture area to be a 25x50 rectangle starting at (0, 0)
-        quad[0].texCoords = sf::Vector2f(0.f, 0.f);
-        quad[1].texCoords = sf::Vector2f(25.f, 0.f);
-        quad[2].texCoords = sf::Vector2f(25.f, 50.f);
-        quad[3].texCoords = sf::Vector2f(0.f, 50.f);
-
-
-
         root->update(delta);
         root->draw(window);
 
