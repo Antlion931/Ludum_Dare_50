66c9d102039f35222f9b29fa7d77371b82a8d532
Grubre <ogrodowczykjakub@gmail.com>
2022-04-03 16:14:16 +0200

    Fixy w WorldView

--- inc/WorldView.hpp
+++ inc/WorldView.hpp
@@ -7,35 +7,38 @@
 #include <map>
 
 
-struct KeyHasher
-{
-    std::size_t operator()(const sf::Vector2i& k) const
-    {
-        return ((std::hash<int>()(k.x) ^ (std::hash<int>()(k.y) << 1)) >> 1);
-    }
-};
+
 
 
 class WorldView : public Node
 {
 private:
+    struct KeyHasher
+    {
+        std::size_t operator()(const sf::Vector2i& k) const
+        {
+            return ((std::hash<int>()(k.x) ^ (std::hash<int>()(k.y) << 1)) >> 1);
+        }
+    };
+
     std::shared_ptr<sf::Texture> tileSet;
     
     std::unordered_map<sf::Vector2i, Chunk, KeyHasher> chunkMap;
 
-    YSort entities;
+    //YSort entities;
     std::shared_ptr<Player> player;
     sf::Vector2i currentCenterCoords;
     
     void chunkChange(sf::Vector2i chunkCoords);
 
-    Chunk allocateChunk(sf::Vector2i chunkCoords, sf::Vector2i relativeTo);
+    void allocateChunk(sf::Vector2i chunkCoords, sf::Vector2i relativeTo);
     void deallocateChunk(sf::Vector2i chunkCoords);
+    void deallocateChunks();
 
 public:
     WorldView(std::shared_ptr<Player> _player, std::shared_ptr<sf::Texture> _tileSet);
 
 protected:
-    void onDraw(sf::RenderTarget &target) override;
+    void onDraw(sf::RenderTarget &target) override; 
     void onUpdate(const sf::Time& delta) override;
 };
