50069714248521bc5392cfb169347ddfd557eac3
Grubre <ogrodowczykjakub@gmail.com>
2022-04-04 18:24:36 +0200

    Dodanie Poisson Disk Sampling (zbugowane, do naprawy)

--- inc/Toolkit.hpp
+++ inc/Toolkit.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <random>
+#include <cmath>
+#include <iostream>
 
 enum LEVEL
 {
@@ -30,4 +33,6 @@ bool clamp(sf::RectangleShape a, sf::Vector2f b);
 bool clamp(sf::RectangleShape a, sf::Vector2i b);
 
 std::ostream& operator<< (std::ostream& out, sf::Vector2f const& vec);
+
+void generatePoints(std::vector<sf::Vector2f> &points, int amountToSpawn, sf::Vector2i topleft, sf::Vector2i bottomright, float minDist = 0);
 
